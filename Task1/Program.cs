8a043ab091708eb29ef0c51a4da8b064465940f4
Roma68 <ipz211_zrr@student.ztu.edu.ua>
2022-11-08 20:17:44 +0200

    Text1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Привіт");
+Console.WriteLine("Привіт");
+Console.WriteLine("Привіт");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
